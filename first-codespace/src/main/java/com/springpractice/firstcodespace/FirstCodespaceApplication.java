cb1531655087175e7de731f9e4a32bdbd7c72cbc
YARISINGU TARUN <118651530+yarisingu@users.noreply.github.com>
2023-05-30 18:06:02 +0000

    myfirst

---
+++ first-codespace/src/main/java/com/springpractice/firstcodespace/FirstCodespaceApplication.java
@@ -0,0 +1,13 @@
+package com.springpractice.firstcodespace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstCodespaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstCodespaceApplication.class, args);
+	}
+
+}
---
+++ first-codespace/src/test/java/com/springpractice/firstcodespace/FirstCodespaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springpractice.firstcodespace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstCodespaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
